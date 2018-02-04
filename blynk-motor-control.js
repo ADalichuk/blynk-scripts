9e26490305fffd4a3c1771a551f01d74be6ba886
ADalichuk <alextropa@gmail.com>
2018-02-04 22:10:44 +0000

    added scripts CO2 control routine

--- blynk-motor-control.js
+++ blynk-motor-control.js
@@ -8,38 +8,70 @@ var powerOnOff = new blynk.VirtualPin(5);
 var inflowFan = new blynk.VirtualPin(0);
 var outflowFan = new blynk.VirtualPin(1);
 var fanSpeedControl = new blynk.VirtualPin(2);
-var fanSpeedPreset = new blynk.VirtualPin(6);
+var presetCO2Level = new blynk.VirtualPin(6);
+var actualCO2Level = new blynk.VirtualPin(7);
 var isPowerOn = 0;
-var inflowFanSpeed = 0;
-var outflowFanSpeed = 0;
+var presetFanSpeed = 50;
+var curInflowFanSpeed = 0;
+var curOutflowFanSpeed = 0;
+var acceptableCO2Level = 10; // %
+var currentCO2Level = 0; // %
+var forceFan = false;
+
+function UpdateFanSpeed()
+{
+  if (1 == isPowerOn)
+  {
+    curInflowFanSpeed = presetFanSpeed;
+    curOutflowFanSpeed = presetFanSpeed;
+    if (true == forceFan)
+    {
+        curInflowFanSpeed = 100;
+        curOutflowFanSpeed = 100;
+	currentCO2Level--;
+    }
+  }
+  else
+  {
+    curInflowFanSpeed = 0;
+    curOutflowFanSpeed = 0;
+  }    
+}
 
 powerOnOff.on('write', function(param){
   isPowerOn = param[0];
   console.log('Power:', isPowerOn);
-
-  if (1 == isPowerOn)
-  {
-    inflowFan.write(inflowFanSpeed);
-    outflowFan.write(outflowFanSpeed);
-  }
-  else
-  {
-    inflowFan.write(0);
-    outflowFan.write(0);
-  }
+  UpdateFanSpeed();
 })
 
 fanSpeedControl.on('write', function(param) {
-  if (1 == isPowerOn)
-  {
-    inflowFan.write(param[0]);
-    outflowFan.write(param[0]);
-    inflowFanSpeed = param[0];
-    outflowFanSpeed = param[0];
-  }
-  else
-  {
-    inflowMotor.write(0);
-    outflowMotor.write(0);
-  }
+    presetFanSpeed = param[0];
+    UpdateFanSpeed();
 });
+
+presetCO2Level.on('write', function(param) {
+    currentCO2Level = param[0];
+});
+
+// Automatically check and supress CO2 level every 2 seconds
+setInterval(function() {
+
+    if (acceptableCO2Level < currentCO2Level)
+    {
+        console.log('Reducing CO2 level:');
+        forceFan = true;
+    }
+    else
+    {
+        forceFan = false;
+    }
+    UpdateFanSpeed();
+    console.log('InflowFan:', curInflowFanSpeed + '%');
+    console.log('OutflowFan:', curOutflowFanSpeed + '%');
+    console.log('CO2:', currentCO2Level + '%');
+    inflowFan.write(curInflowFanSpeed);
+    outflowFan.write(curOutflowFanSpeed);
+    actualCO2Level.write(currentCO2Level);
+}, 2000);
+
+
