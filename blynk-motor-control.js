70672d30395911a4e37b570e0bb72d7fe1a969cf
oleksii.dalichuk <oleksii.dalichuk@envisiontec.de>
2018-02-04 21:37:21 +0200

    minor fixes in blynk-motor-control.js

--- blynk-motor-control.js
+++ blynk-motor-control.js
@@ -1,37 +1,41 @@
-var Blynk = require('blynk-library')
+var Blynk = require('blynk-library');
 
-var AUTH = 'd363e570a1664e398a72a591276cd965'
+var AUTH = 'd363e570a1664e398a72a591276cd965';
 
-var blynk = new Blynk.Blynk(AUTH)
+var blynk = new Blynk.Blynk(AUTH);
 
-var powerOnOff = new blynk.VirtualPin(5)
-var inflowFan = new blynk.VirtualPin(0)
-var outflowFan = new blynk.VirtualPin(1)
-var fanSpeedControl = new blynk.VirtualPin(2)
-var fanSpeedPreset = new blynk.VirtualPin(6)
-var isPowerOn = false
-var inflowFanSpeed = 0
-var outflowFanSpeed = 0
+var powerOnOff = new blynk.VirtualPin(5);
+var inflowFan = new blynk.VirtualPin(0);
+var outflowFan = new blynk.VirtualPin(1);
+var fanSpeedControl = new blynk.VirtualPin(2);
+var fanSpeedPreset = new blynk.VirtualPin(6);
+var isPowerOn = 0;
+var inflowFanSpeed = 0;
+var outflowFanSpeed = 0;
 
 powerOnOff.on('write', function(param){
-  isPowerOn = Boolean(param[0])
-  if (isPowerOn)
+  isPowerOn = param[0];
+  console.log('Power:', isPowerOn);
+
+  if (1 == isPowerOn)
   {
-    inflowMotor.write(inflowFanSpeed)
-    outflowMotor.write(outflowFanSpeed)
-    return
+    inflowFan.write(inflowFanSpeed);
+    outflowFan.write(outflowFanSpeed);
   }
-  inflowMotor.write(0)
-  outflowMotor.write(0)
+  else
+  {
+    inflowFan.write(0);
+    outflowFan.write(0);
+  }
 })
 
-motorPowerControl.on('write', function(param) {
-  if (isPowerOn)
+fanSpeedControl.on('write', function(param) {
+  if (1 == isPowerOn)
   {
-    inflowMotor.write(param[0])
-    outflowMotor.write(param[0])
-    inflowFanSpeed = param[0]
-    outflowFanSpeed = param[0]
+    inflowFan.write(param[0]);
+    outflowFan.write(param[0]);
+    inflowFanSpeed = param[0];
+    outflowFanSpeed = param[0];
   }
   else
   {
